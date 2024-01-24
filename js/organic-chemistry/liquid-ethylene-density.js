d9f1359315ae058a4b8e5cd6b5ea38855f1142ac
shinus <shinusolanki76@gmail.com>
2024-01-24 09:44:44 +0530

    updating

--- js/organic-chemistry/liquid-ethylene-density.js
+++ js/organic-chemistry/liquid-ethylene-density.js
@@ -2,21 +2,76 @@
 let input1 = document.getElementById("input1");
 let input2 = document.getElementById("input2");
 
+var output = document.getElementById("result-section");
+var calcBtn = document.getElementById("calculate_btn");
 
-let calcBtn = document.getElementById("calcBtn");
-let result = document.getElementById("result-section");
+const getScript = document.currentScript;
+const permaLink = getScript.dataset.permalink;
 
-calcBtn.style.background = "#f8320df7";
+var queryParams = [
+  { name: "weight", values: input1 },
+  { name: "volume", values: input2 },
+];
+
+
+function setParamValues(queryParams) {
+  const params = new URLSearchParams(window.location.search);
+  for (queryP of queryParams) {
+    var parameter_value = params.get(queryP.name);
+    if (queryP.values.tagName == "INPUT") {
+      queryP.values.value = parameter_value;
+    } else if (queryP.values.tagName == "SELECT") {
+      queryP.values.selectedIndex = parameter_value;
+    }
+  }
+}
+
+function init() {
+  var url = window.location.href;
+  if (url.includes("?")) {
+    setParamValues(queryParams);
+    showResult();
+  }
+}
+
+init();
 
 
 input1.placeholder = 'Enter mass in kg'
 input2.placeholder = 'Enter volume in m3'
-calcBtn.addEventListener('click',calculate)
 
 function calculate(){
     let weightI = Number(input1.value)
     let volumeI = Number(input2.value)
     let destiny = weightI / volumeI
-    result.textContent = `${destiny} kg per cubic meter`
+    result = destiny
+    return result
 }
+
+function showResult() {
+    if (event && event.type == "click") {
+      reloadPage(queryParams);
+      return;
+    }
+    var result = calculate();
+  
+    var div1 = document.createElement("div");
+    var div2 = document.createElement("div");
+    var div3 = document.createElement("div");
+    var div4 = document.createElement("div");
+    var div5 = document.createElement("div");
+  
+    output.innerHTML = "";
+  
+    div1.innerHTML = "<b>Density  " + result.toFixed(2)  + " kg per cubic meter  </b>";
+  
+    output.append(div1);
+    output.append(div2);
+    output.append(div3);
+    output.append(div4);
+    output.append(div5);
+  }
+  
+  calcBtn.addEventListener("click", showResult);
+  
 
--- js/organic-chemistry/radiocarbon-dating.js
+++ js/organic-chemistry/radiocarbon-dating.js
@@ -1,21 +1,72 @@
-
 let input1 = document.getElementById("input1");
 let input2 = document.getElementById("input2");
+var output = document.getElementById("result-section");
+var calcBtn = document.getElementById("calculate_btn");
+
+const getScript = document.currentScript;
+const permaLink = getScript.dataset.permalink;
+
+var queryParams = [
+  { name: "percent", values: input1 },
+  { name: "halflife", values: input2 },
+];
 
 
-let calcBtn = document.getElementById("calcBtn");
-let result = document.getElementById("result-section");
+function setParamValues(queryParams) {
+  const params = new URLSearchParams(window.location.search);
+  for (queryP of queryParams) {
+    var parameter_value = params.get(queryP.name);
+    if (queryP.values.tagName == "INPUT") {
+      queryP.values.value = parameter_value;
+    } else if (queryP.values.tagName == "SELECT") {
+      queryP.values.selectedIndex = parameter_value;
+    }
+  }
+}
 
-calcBtn.style.background = "#f8320df7";
+function init() {
+  var url = window.location.href;
+  if (url.includes("?")) {
+    setParamValues(queryParams);
+    showResult();
+  }
+}
 
-calcBtn.addEventListener('click',calculate)
+init();
 
 function calculate() {
-    const percentRemaining = parseFloat(input1.value); 
-    const halfLife = parseFloat(input2.value);
+  const percentRemaining = parseFloat(input1.value);
+  const halfLife = parseFloat(input2.value);
 
-    const k = Math.log(2) / halfLife;
-    const t = Math.log(percentRemaining / 100) / k;
+  const k = Math.log(2) / halfLife;
+  const t = Math.log(percentRemaining / 100) / k;
 
-    result.textContent = t.toFixed(2);
+ return result = t.toFixed(2);
+}
+
+function showResult() {
+  if (event && event.type == "click") {
+    reloadPage(queryParams);
+    return;
   }
+  var result = calculate();
+
+  var div1 = document.createElement("div");
+  var div2 = document.createElement("div");
+  var div3 = document.createElement("div");
+  var div4 = document.createElement("div");
+  var div5 = document.createElement("div");
+
+  output.innerHTML = "";
+
+  div1.innerHTML =
+    "<b>radiocarbon age  " + result  +"  </b>";
+
+  output.append(div1);
+  output.append(div2);
+  output.append(div3);
+  output.append(div4);
+  output.append(div5);
+}
+
+calcBtn.addEventListener("click", showResult);
